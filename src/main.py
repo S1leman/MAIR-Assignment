fbb4606b276fca24a3e35c049115f9c5ece978fb
Slleman <silemanpatang@gmail.com>
2025-09-09 14:57:27 +0200

    added function

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-# file = open('data/dialog_acts.dat', 'r')
-# content = file.read()
-# print(content)
-regels = []
-dialogue_act = []
-utterance = []
-with open('data/dialog_acts.dat', 'r') as f:
-    lines = f.readlines()
-    for line in lines[:3]:
-        data = line.strip().lower().split(' ', 1)
-        regels.append(data)
-        dialogue_act.append(data[0])
-        utterance.append(data[1])
+def read_data():
+    regels = [] # can be removed, used for testing
+    dialogue_act = []
+    utterance = []
+    with open('data/dialog_acts.dat', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            data = line.strip().lower().split(' ', 1)
+            regels.append(data) # can be removed, used for testing
+            dialogue_act.append(data[0])
+            utterance.append(data[1])
 
-print(regels)
-print(dialogue_act)
-print(utterance)
+    #print(regels)
+    #print(dialogue_act)
+    #print(utterance)
